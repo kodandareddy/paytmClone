186803c2277b24bbfb4b810a531a943827e195bf
Kodanda <kodandareddy21@gmail.com>
2022-08-28 18:29:52 +0530

    repso changes

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'skyblue': '#2CC6F4',
+        'gray':'#F4F4F5',
+        'skyblueHover':'#00afe3',
+        "darkblue":'#0f4a8a',
+        "darkblueHover":"#0D3E80"
+      },
+    },
+  },
+  plugins: [],
+}
